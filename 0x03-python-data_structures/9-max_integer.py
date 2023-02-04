10f212dce130da36e24ff5a3c55ba810f2d46a0c
annanharry <annanharry7@gmail.com>
2023-02-04 01:10:34 -0800

    hmm

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
-def multiple_returns(sentence):
-    if len(sentence) > 0:
-        return tuple([len(sentence), sentence[0]])
-    else:
-        return tuple([0, None])
+def max_integer(my_list=[]):
+    if len(my_list) == 0:
+        return ("None")
+    x = my_list[0]
+    for i in my_list:
+        if i > x:
+            x = i
+    return (x)
